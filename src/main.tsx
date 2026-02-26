e7bf381031ccaa14c599f0f241c37ceb496d9980
werbo <bandon1016@gmail.com>
2026-02-26 23:58:56 +0800

    feat: add console warning message for developers

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+console.log(
+  '%c測試玩法中，歡迎自由探索，但請不要使用暗黑玩法，謝謝配合！',
+  'color: #fbbf24; font-size: 16px; font-weight: bold; background: #000; padding: 10px; border-radius: 5px;'
+);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
